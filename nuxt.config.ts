73eb1720b65c195c3275c38c5b6be0620ab252d7
Ceci Olivera <ceci.coding@gmail.com>
2025-07-19 13:42:51 +0200

    feat(SEO): title, description and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/image'],
+  app: {
+    head: {
+      title: 'Planastico',
+      meta: [
+        { name: 'description', content: 'Tu app de planes en Granada' }
+      ]
+    }
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
