7473aba8654c64f44170be404ac9d14660cbda2a
Ceci Olivera <ceci.coding@gmail.com>
2025-10-18 20:49:23 +0200

    fix: BEM classes for base input

--- components/Form/FormBaseInput.vue
+++ components/Form/FormBaseInput.vue
@@ -20,32 +20,38 @@ const id = useUuid();
 </script>
 
 <template>
-  <div class="base-input">
+  <div class="form-base-input">
     <FormBaseInputLabel :input-id="id" :label="props.field.label.name" :is-visible="props.field.label.isVisible" />
-    <div class="base-input__wrapper">
-      <input :id="id" :value="props.modelValue" class="base-input__input" :class="[
-        `base-input__input--${props.field.roundedCorner}`,
-        {
-          'base-input__input--has-error': props.error.length,
-          'base-input__input--has-trailing-icon': props.field.isCurrency || props.field.isErasable
-        }
-      ]" :type="props.field.inputType" :placeholder="props.field.placeholder"
+    <div class="form-base-input__wrapper">
+      <input
+        :id="id"
+        :value="props.modelValue"
+        class="form-base-input__input"
+        :class="[
+          `form-base-input__input--${props.field.roundedCorner}`,
+          {
+            'form-base-input__input--has-error': props.error.length,
+            'form-base-input__input--has-trailing-icon': props.field.isCurrency || props.field.isErasable
+          }
+        ]"
+        :type="props.field.inputType"
+        :placeholder="props.field.placeholder"
         @input="(e) => emit('update:model-value', e.target.value)">
-      <span v-if="props.field.isCurrency" class="base-input__currency">
+      <span v-if="props.field.isCurrency" class="form-base-input__currency">
         €
       </span>
     </div>
-    <span v-if="props.field.maxLength && !props.error.length" class="base-input__count">
+    <span v-if="props.field.maxLength && !props.error.length" class="form-base-input__count">
       {{ props.modelValue.length }}/{{ props.field.maxLength }}
     </span>
-    <span v-if="props.error.length" class="base-input__error">
+    <span v-if="props.error.length" class="form-base-input__error">
       {{ props.error }}
     </span>
   </div>
 </template>
 
 <style scoped lang="scss">
-.base-input {
+.form-base-input {
   display: flex;
   flex-direction: column;
   width: 100%;
@@ -61,7 +67,7 @@ const id = useUuid();
     background-color: var(--planastico-warm-soft-gray);
     padding: 0 1rem;
     border: none;
-    border-bottom: 6px solid var(--planastico-cold-black);
+    border-bottom: var(--planastico-border-xl);
 
     &--left {
       border-radius: 14px 0 0 0;
@@ -72,7 +78,7 @@ const id = useUuid();
     }
 
     &--has-error {
-      border-bottom: 6px solid var(--planastico-error-red);
+      border-color: var(--planastico-error-red);
       color: var(--planastico-error-red);
     }
 
@@ -84,7 +90,7 @@ const id = useUuid();
       outline-color: var(--planastico-error-red);
     }
 
-    &--has-error+.base-input__currency {
+    &--has-error + .form-base-input__currency {
       color: var(--planastico-error-red);
     }
   }
@@ -122,11 +128,5 @@ input::-webkit-inner-spin-button {
 /* Firefox */
 input[type=number] {
   -moz-appearance: textfield;
-}
-
-@media screen and (min-width: 768px) {
-  .base-input {
-    width: 250px;
-  }
 }
 </style>
